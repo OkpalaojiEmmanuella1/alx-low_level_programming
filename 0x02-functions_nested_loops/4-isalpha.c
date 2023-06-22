d4d5763d314b62568a2466b5a7e129213d6eeba7
OkpalaojiEmmanuella1 <okpalaojiemmanuell@gmail.com>
2023-06-21 17:08:46 -0700

    love never fails

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Shows 1 if the input is A
+ * @c: The character in ASCII code
+ * Return: 1 for letters.  0 for the rest
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 65  && c  <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	else
+	{	
+		return (0);
+	}
+	_putchar('\n');
+}
+
